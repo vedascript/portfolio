ca13b416594108d3abd0ad3959be551118479568
Vedansh Wani <me.vedanshwani@gmail.com>
2021-07-18 16:38:25 +0530

    portfolio draft 1

---
+++ src/utils/helperList.js
@@ -0,0 +1,138 @@
+export const showcaseSkills = [
+  {
+    label: "Javascript",
+    rating: 71,
+  },
+  {
+    label: "Node JS",
+    rating: 81,
+  },
+  {
+    label: "React",
+    rating: 76,
+  },
+  {
+    label: "Mongo dB",
+    rating: 72,
+  },
+  {
+    label: "Redux",
+    rating: 73,
+  },
+  {
+    label: "Express",
+    rating: 74,
+  },
+  {
+    label: "HTML CSS",
+    rating: 85,
+  },
+];
+
+export const skillsList = [
+  {
+    label: "Javascript",
+    src: "https://img.icons8.com/color/144/000000/javascript.png",
+  },
+  {
+    label: "React",
+    src: "https://img.icons8.com/color/144/000000/react-native.png",
+  },
+  {
+    label: "Redux",
+    src: "https://img.icons8.com/color/144/000000/redux.png",
+  },
+  {
+    label: "Node JS",
+    src: "https://img.icons8.com/color/144/000000/nodejs.png",
+  },
+  {
+    label: "Express JS",
+    src: "https://i.ibb.co/9smDM6j/pngegg.png",
+  },
+  {
+    label: "Mongo dB",
+    src: "https://i.ibb.co/ZVh01dm/rsz-mong2.png",
+  },
+  {
+    label: "My Sql",
+    src: "https://img.icons8.com/fluent/96/000000/mysql-logo.png",
+  },
+  {
+    label: "Firebase",
+    src: "https://img.icons8.com/color/50/000000/firebase.png",
+  },
+  {
+    label: "HTML5",
+    src: "https://img.icons8.com/color/96/000000/html-5--v1.png",
+  },
+  {
+    label: "CSS3",
+    src: "https://img.icons8.com/color/96/000000/css3.png",
+  },
+  {
+    label: "Github",
+    src: "https://img.icons8.com/fluent/144/000000/github.png",
+  },
+  {
+    label: "NPM",
+    src: "https://img.icons8.com/color/144/000000/npm.png",
+  },
+  {
+    label: "Yarn",
+    src: "https://i.ibb.co/GtbG98V/yarn2.png",
+  },
+  {
+    label: "Material UI",
+    src: "https://material-ui.com/static/logo_raw.svg",
+  },
+  {
+    label: "Bootstrap",
+    src: "https://i.ibb.co/zx0Tt9M/bootstrap.png",
+  },
+];
+
+export const projectsList = [
+  {
+    label: "Coursera Clone",
+    imgSrc:
+      "https://user-images.githubusercontent.com/42139446/122419364-79cf0d80-cfa8-11eb-8aa3-66e99eebfdf4.png",
+    src: "#",
+    githubSrc: "https://github.com/vedascript/coursera-clone/tree/master",
+    description: "An EdTech Platform with all MVP features",
+    stack: ["React", "Redux", "Express", "Mongoose", "Node.js", "Material-UI"],
+  },
+  {
+    label: "Blog Platform",
+    imgSrc: "blog-site.png",
+    src: "",
+    githubSrc: "https://github.com/vedascript/blog-site",
+    description:
+      "A Blog platform where user can publish,edit,delete his blogs and can see other user's blogs",
+    stack: ["React", "Redux", "Express", "Mongoose", "Node.js", "CSS"],
+  },
+  {
+    label: "Expense Tracker",
+    imgSrc: "expenses.png",
+    src: "#",
+    githubSrc: "#",
+    description: "An expense tracker to track user's expenses",
+    stack: ["React", "React-Charts", "Material-UI"],
+  },
+  {
+    label: "Sudoku Solver",
+    imgSrc: "sudoku.png",
+    src: "https://sudokusolvervw.netlify.app",
+    githubSrc: "https://github.com/vedascript/sudokuSolver",
+    description: "A automated sudoku solver - an aaplication of backtraking",
+    stack: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    label: "Fathom Health Clone",
+    imgSrc: "fathom.png",
+    src: "https://fathomhealth.netlify.app",
+    githubSrc: "https://github.com/vedascript/fathomhealth-clone",
+    description: "A visual clone of Fathom Health completed in 3 days",
+    stack: ["JavaScript", "HTML", "CSS"],
+  },
+];
